7b207ed99fb11371ee071fe2094311339a38cff9
Chisaki <fujimiya.chisaki@gmail.com>
2026-02-14 23:16:48 +0800

    C++ version for Arduino import
    Feb. 14, 2026 : initial changes
    - issue on position not reaching target values

--- main.cpp
+++ main.cpp
@@ -6,18 +6,23 @@
 using namespace std;
 using namespace Eigen;
 
-
-MatrixXd jacobian(VectorXd parameter) {
-    Matrix<double, 2, 1 > jacobian;
-    float theta1 = parameter[3];
-    float theta2 = parameter[3] + parameter[4];
-    float theta3 = parameter[3] + parameter[4] + parameter[5];
+MatrixXd Jacobian(VectorXd parameter) {
+    Matrix<double, 2, 3 > jacobian;
+    double theta1 = parameter[3];
+    double theta2 = parameter[3] + parameter[4];
+    double theta3 = parameter[3] + parameter[4] + parameter[5];
     
-    jacobian << 
+    jacobian <<
+    //dx/dt
+    parameter[0]*sin(theta1)+parameter[1]*sin(theta2)+parameter[2]*sin(theta3), 
+    parameter[0]*sin(theta1)+parameter[1]*sin(theta2),
+    parameter[2]*sin(theta3),
+    //dy/dt
     parameter[0]*cos(theta1)+parameter[1]*cos(theta2)+parameter[2]*cos(theta3),
-    parameter[0]*sin(theta1)+parameter[1]*sin(theta2)+parameter[2]*sin(theta3);
+    parameter[0]*cos(theta1)+parameter[1]*cos(theta2),
+    parameter[2]*cos(theta3);
 
-    float err = 1e-3;
+    double err = 1e-3;
     for (int i = 0; i < jacobian.size(); i++) {
         if (jacobian(i) < err) {
             jacobian(i) = 0;
@@ -26,30 +31,57 @@ MatrixXd jacobian(VectorXd parameter) {
     return jacobian;
 }
 
-MatrixXd pseudoinverse(VectorXd parameter) {
-    MatrixXd J = jacobian(parameter);
-    MatrixXd JT = J.transpose();
-    MatrixXd JJT = J * JT;
-    MatrixXd inv = JJT.inverse();
-    //MatrixXd res = inv * JT;
-    /*
-    cout << J << endl;
-    cout << JT << endl;
-    */
-    return inv;
+MatrixXd position(VectorXd parameter) {
+    Matrix<double, 2,1> position;
+    double theta1 = parameter[3]; double theta2 = parameter[3] + parameter[4];
+    double theta3 = parameter[3] + parameter[4] + parameter[5];
+    
+    position<<
+    //x
+    parameter[0]*cos(theta1)+parameter[1]*cos(theta2)+parameter[2]*cos(theta3),
+    //y
+    parameter[0]*sin(theta1)+parameter[1]*sin(theta2)+parameter[2]*sin(theta3);
+    return position;
 }
 
-class Model {
-    public:
-        VectorXd links;
-        VectorXd angle;
-        VectorXd target;
-};
+MatrixXd pseudoinverse(VectorXd parameter, int iteration, MatrixXd initial_p, MatrixXd error) {
+    //initiating iteration values
+    MatrixXd Jacobian_iter = Jacobian(parameter); MatrixXd initial_iter = initial_p;
+    MatrixXd JacobianT = Jacobian_iter.transpose();
+    Matrix<double, 3, 1> angle_iter;
+    angle_iter << parameter[3], parameter[4], parameter[5];
+
+    //Damping values to avoid singularity
+    double alpha = 0.01; double lambda = 1e-5;
+    MatrixXd Ide = MatrixXd::Identity(Jacobian_iter.cols(), Jacobian_iter.cols());
+    MatrixXd dls = lambda * lambda * Ide;
+
+    //Looping angle to approximate target location with the angle
+    MatrixXd rem;
+    for (int i = 0; i < iteration+1; i ++) {
+        VectorXd temp(6); temp << 8.0e+0, 5.0e+0, 3.0e+0, angle_iter[0], angle_iter[1], angle_iter[2];
+
+        rem = alpha*((((JacobianT * Jacobian_iter) + dls).inverse() * JacobianT)*error);
+        angle_iter = angle_iter + rem;
+
+        Jacobian_iter = Jacobian(temp); 
+        JacobianT = Jacobian_iter.transpose();
+        initial_iter = position(temp);
+
+        Vector3d angle_deg = angle_iter * 180.0 / M_PI;
+        Vector2d current_position = position(temp);
+        cout << "Iteration: " << i 
+            << " , angles: " << angle_deg.transpose() 
+            << " , position: " << current_position.transpose() << endl;
+    }
+    return rem;
+}
 
 int main() {
     double PI = 3.141592653589;
-    VectorXd parameter(6); parameter << 8.0, 5.0, 3.0, 90.0*PI/180.0, 0.0, 0.0;
-    
-    cout << pseudoinverse(parameter) << endl;;
+    VectorXd parameter(6); parameter << 8.0, 5.0, 3.0, 45.0*PI/180.0, 45.0*PI/180.0, 45.0*PI/180.0;
+    MatrixXd initial_p = position(parameter);
+    Matrix<double, 2, 1> Target; Target << 10.0, 12.0;
+    pseudoinverse(parameter, 100, initial_p, Target - initial_p);
     return 1; 
 }
